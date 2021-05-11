d079f4e355fe72733950fb401c60c442e033fe58
Ricardo Arian Ortega Garcia <40581565+rortega-sps@users.noreply.github.com>
2021-05-11 11:46:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          ADIOS.
         </p>
         <a
           className="App-link"
